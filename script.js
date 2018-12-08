917139c8c5f8b370d481671ea213ba4ec5e1dd4b
ferdowsirumi <45703439+ferdowsirumi@users.noreply.github.com>
2018-12-07 21:39:42 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function openpic() {
 
-    document.getElementById("mira").src = "/Users/altafhussain/Downloads/app5.jpg";
+    document.getElementById("mira").src = "img/app5.jpg";
 }
 
 
 function openout() {
 
-    document.getElementById("sumi").src = "/Users/altafhussain/Downloads/img_app2.jpg";
+    document.getElementById("sumi").src = "img/img_app2.jpg";
     document.getElementById("sumi").style.height = "400px";
 }
 
 
 function openend() {
 
-    document.getElementById("moni").src = "/Users/altafhussain/Downloads/app5.jpg";
+    document.getElementById("moni").src = "img/app5.jpg";
 
 }
 
