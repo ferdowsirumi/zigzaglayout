7bcd49bc8cd956161ec23b87cf3e5f100ce26be5
ferdowsirumi <45703439+ferdowsirumi@users.noreply.github.com>
2018-12-07 21:12:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function openpic() {
+
+    document.getElementById("mira").src = "/Users/altafhussain/Downloads/app5.jpg";
+}
+
+
+function openout() {
+
+    document.getElementById("sumi").src = "/Users/altafhussain/Downloads/img_app2.jpg";
+    document.getElementById("sumi").style.height = "400px";
+}
+
+
+function openend() {
+
+    document.getElementById("moni").src = "/Users/altafhussain/Downloads/app5.jpg";
+
+}
+
+
+function myfunction() {
+
+    document.getElementById("demo").innerHTML = "Its my first experience to write Zig Zag Layout example";
+    document.getElementById("demo").style.color= "red";
+   document.getElementById("demo").style.borderBottom= "3px solid red";
+
+
+
+}
+
+
